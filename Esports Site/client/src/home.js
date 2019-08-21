90081180b5e06ec7dcfa536e9ddf07faa914ab35
agtrevino <39744373+agtrevino@users.noreply.github.com>
2019-08-20 23:18:34 -0500

    added navbar

--- Esports Site/client/src/home.js
+++ Esports Site/client/src/home.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import SignUp from './createUser';
-import {BrowserRouter, Route} from 'react-router-dom';
-import { Jumbotron, Container} from 'reactstrap';
+
+// UI Components
+
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Jumbotron, Container } from 'reactstrap';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 
 const title = {
-    fontSize: '50px',
-    color: 'red',
+  fontSize: '50px',
+  color: 'red',
 }
-export default class Home extends Component { 
-  state = { 
+export default class Home extends Component {
+  state = {
     data: [],
     id: 0,
     message: null,
@@ -19,7 +27,7 @@ export default class Home extends Component {
     idToUpdate: null,
     objectToUpdate: null,
   }
- // when component mounts, first thing it does is fetch all existing data in our db
+  // when component mounts, first thing it does is fetch all existing data in our db
   // then we incorporate a polling logic so that we can easily see if our db has
   // changed and implement those changes into our UI
   componentDidMount() {
@@ -106,19 +114,29 @@ export default class Home extends Component {
   render() {
     const { data } = this.state;
     return (
+
       <BrowserRouter>
-      <div id='container'>
-           <a href="signup">Sign Up</a>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Button variant="outlined" href="#outlined-buttons">
+              Sign Up</Button>
+          </Toolbar>
+        </AppBar>
+        <div id='container'>
+          <a href="signup">Sign Up</a>
         </div>
         <div>
-      <Jumbotron fluid>
-        <Container fluid>
-          <h1 className="display-3">Fluid jumbotron</h1>
-          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-        </Container>
-      </Jumbotron>
-    </div>
-      <div>
+          <Jumbotron fluid>
+            <Container fluid>
+              <h1 className="display-3">Fluid jumbotron</h1>
+              <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </Container>
+          </Jumbotron>
+        </div>
+        <div>
           <br>
           </br>
           <br>
@@ -134,67 +152,67 @@ export default class Home extends Component {
           </br><br>
           </br><br>
           </br>
-        <Route exact={true} path='/signin' render={() => (
+          <Route exact={true} path='/signin' render={() => (
             <div className="App">
               <createUser />
             </div>
-          )}/>
-        <ul>
-          {data.length <= 0
-            ? 'NO DB ENTRIES YET'
-            : data.map((dat) => (
+          )} />
+          <ul>
+            {data.length <= 0
+              ? 'NO DB ENTRIES YET'
+              : data.map((dat) => (
                 <li style={{ padding: '10px' }} key={data.message}>
                   <span style={{ color: 'gray' }}> id: </span> {dat.id} <br />
                   <span style={{ color: 'gray' }}> data: </span>
                   {dat.message}
                 </li>
               ))}
-        </ul>
-        <div style={{ padding: '10px' }}>
-          <input
-            type="text"
-            onChange={(e) => this.setState({ message: e.target.value })}
-            placeholder="add something in the database"
-            style={{ width: '200px' }}
-          />
-          <button onClick={() => this.putDataToDB(this.state.message)}>
-            ADD
+          </ul>
+          <div style={{ padding: '10px' }}>
+            <input
+              type="text"
+              onChange={(e) => this.setState({ message: e.target.value })}
+              placeholder="add something in the database"
+              style={{ width: '200px' }}
+            />
+            <button onClick={() => this.putDataToDB(this.state.message)}>
+              ADD
           </button>
-        </div>
-        <div style={{ padding: '10px' }}>
-          <input
-            type="text"
-            style={{ width: '200px' }}
-            onChange={(e) => this.setState({ idToDelete: e.target.value })}
-            placeholder="put id of item to delete here"
-          />
-          <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>
-            DELETE
+          </div>
+          <div style={{ padding: '10px' }}>
+            <input
+              type="text"
+              style={{ width: '200px' }}
+              onChange={(e) => this.setState({ idToDelete: e.target.value })}
+              placeholder="put id of item to delete here"
+            />
+            <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>
+              DELETE
           </button>
-        </div>
-        <div style={{ padding: '10px' }}>
-          <input
-            type="text"
-            style={{ width: '200px' }}
-            onChange={(e) => this.setState({ idToUpdate: e.target.value })}
-            placeholder="id of item to update here"
-          />
-          <input
-            type="text"
-            style={{ width: '200px' }}
-            onChange={(e) => this.setState({ updateToApply: e.target.value })}
-            placeholder="put new value of the item here"
-          />
-          <button
-            onClick={() =>
-              this.updateDB(this.state.idToUpdate, this.state.updateToApply)
-            }
-          >
-            UPDATE
+          </div>
+          <div style={{ padding: '10px' }}>
+            <input
+              type="text"
+              style={{ width: '200px' }}
+              onChange={(e) => this.setState({ idToUpdate: e.target.value })}
+              placeholder="id of item to update here"
+            />
+            <input
+              type="text"
+              style={{ width: '200px' }}
+              onChange={(e) => this.setState({ updateToApply: e.target.value })}
+              placeholder="put new value of the item here"
+            />
+            <button
+              onClick={() =>
+                this.updateDB(this.state.idToUpdate, this.state.updateToApply)
+              }
+            >
+              UPDATE
           </button>
+          </div>
         </div>
-      </div>
-      </BrowserRouter>
+      </BrowserRouter >
     );
   }
 }
